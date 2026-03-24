1544d6939e07be48b47a2f0aa5054ef57bdafa37
WUTLeader <igthedou@qq.com>
2026-03-24 21:56:09 +0800

    Add files via upload

---
+++ global.h
@@ -0,0 +1,9 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+//宏定义
+#define FALSE 0
+#define TRUE 1
+
+#endif // !GLOBAL_H
+
---
+++ tool.h
@@ -0,0 +1,7 @@
+#ifndef TOOL_H
+#define TOOL_H
+
+void timeToString(time_t t, char* pBuf);
+
+#endif // !TOOL_H
+
